6c564d4f66fc68623a94ca13f7d44c30ba1fa06a
lil-pepe1 <oscar.mikaoj@gmail.com>
2023-04-05 03:29:58 +0200

    Added additional responsivnes + main + footer

--- js/index.js
+++ js/index.js
@@ -1,12 +1,39 @@
 const burgerMenu = document.querySelector(".header__burger-menu");
-const toggleMenu = document.querySelector(".header__img");
+const openMenu = document.querySelector(".header__img-open");
+const closeMenu = document.querySelector(".header__img-close");
 
-toggleMenu.addEventListener("click", () => {
+openMenu.addEventListener("click", () => {
   const visibility = burgerMenu.getAttribute("data-visible");
 
   if (visibility === "false") {
     burgerMenu.setAttribute("data-visible", true);
-  } else if (visibility === "true") {
+    openMenu.style.display = "none";
+  }
+});
+
+closeMenu.addEventListener("click", () => {
+  const visibility = burgerMenu.getAttribute("data-visible");
+
+  if (visibility === "true") {
     burgerMenu.setAttribute("data-visible", false);
+    openMenu.style.display = "flex";
+  }
+});
+
+const openSearch = document.getElementById("search");
+const closeSearch = document.querySelector(".search-modal__btn");
+const searchModalOverlay = document.querySelector(".search-modal-overlay");
+
+openSearch.addEventListener("click", () => {
+  searchModalOverlay.style.display = "flex";
+});
+
+closeSearch.addEventListener("click", () => {
+  searchModalOverlay.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+  if (event.target == searchModalOverlay) {
+    searchModalOverlay.style.display = "none";
   }
 });
